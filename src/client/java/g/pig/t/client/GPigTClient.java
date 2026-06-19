4c643a8376586ef07392cf4910927d646983990c
Paul Smith <paul@pafsmith.dev>
2026-06-19 10:26:12 +0100

    init

---
+++ src/client/java/g/pig/t/client/GPigTClient.java
@@ -0,0 +1,10 @@
+package g.pig.t.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GPigTClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
