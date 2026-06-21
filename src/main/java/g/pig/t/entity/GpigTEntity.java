a17497e692c400c0f45070042c36e11eb00b8761
Cameron Johnson-Peart <89163342+CJohnsonPeart25@users.noreply.github.com>
2026-06-21 02:20:00 +0100

    feat: assign random name to GpigT on spawn
    Override finalizeSpawn to pick a random name from an inline list and
    show it above the entity. Fires on spawn egg and /summon but not on
    load from disk.

--- src/main/java/g/pig/t/entity/GpigTEntity.java
+++ src/main/java/g/pig/t/entity/GpigTEntity.java
@@ -1,11 +1,43 @@
 package g.pig.t.entity;
 
+import java.util.List;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.animal.pig.Pig;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.jetbrains.annotations.Nullable;
 
 public class GpigTEntity extends Pig {
+    private static final List<String> NAMES = List.of(
+            "Sir Oinksalot",
+            "Hammington",
+            "Truffle",
+            "Bacon Bit",
+            "Snortimer",
+            "Lord Squeals",
+            "Porky",
+            "Mudpuddle",
+            "Curly",
+            "Princess Trotter"
+    );
+
     public GpigTEntity(EntityType<? extends Pig> entityType, Level level) {
         super(entityType, level);
+    }
+
+    @Override
+    @Nullable
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty,
+                                        EntitySpawnReason reason, @Nullable SpawnGroupData spawnGroupData) {
+        SpawnGroupData data = super.finalizeSpawn(level, difficulty, reason, spawnGroupData);
+        String name = NAMES.get(this.getRandom().nextInt(NAMES.size()));
+        this.setCustomName(Component.literal(name));
+        this.setCustomNameVisible(true);
+        return data;
     }
 }
