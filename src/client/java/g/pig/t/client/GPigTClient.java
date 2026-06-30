b273971e20038638b535b2f9a2e7af6768ac1857
Paul Smith <paul@pafsmith.dev>
2026-06-30 13:14:43 +0100

    feat: bind GPigT entity texture

--- src/client/java/g/pig/t/client/GPigTClient.java
+++ src/client/java/g/pig/t/client/GPigTClient.java
@@ -3,11 +3,10 @@ package g.pig.t.client;
 import g.pig.t.registry.GPigTEntities;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.renderer.entity.PigRenderer;
 
 public class GPigTClient implements ClientModInitializer {
 	@Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(GPigTEntities.GPIGT, PigRenderer::new);
+        EntityRendererRegistry.register(GPigTEntities.GPIGT, GpigTRenderer::new);
     }
 }
