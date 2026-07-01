f24d693bde412db0551bffe596c79a056f6527ac
Paul Smith <paul@pafsmith.dev>
2026-07-01 09:29:24 +0100

    feat: add sign state attachment

---
+++ src/main/java/g/pig/t/sign/SignState.java
@@ -0,0 +1,24 @@
+package g.pig.t.sign;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.util.StringRepresentable;
+
+public enum SignState implements StringRepresentable {
+    NONE("none"),
+    QUESTION("question"),
+    CLAIMED("claimed"),
+    ANSWER("answer");
+
+    public static final Codec<SignState> CODEC = StringRepresentable.fromEnum(SignState::values);
+
+    private final String serializedName;
+
+    SignState(String serializedName) {
+        this.serializedName = serializedName;
+    }
+
+    @Override
+    public String getSerializedName() {
+        return serializedName;
+    }
+}
