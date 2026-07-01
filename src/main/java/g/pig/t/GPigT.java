7bc8873c3bf8cebc3515765e9290bccb48f4579e
Paul Smith <paul@pafsmith.dev>
2026-07-01 09:30:04 +0100

    feat: add sign state debug command

--- src/main/java/g/pig/t/GPigT.java
+++ src/main/java/g/pig/t/GPigT.java
@@ -4,6 +4,7 @@ import g.pig.t.names.NamesReloadListener;
 import g.pig.t.registry.GPigTEntities;
 import g.pig.t.registry.GPigTItems;
 import g.pig.t.sign.SignAttachments;
+import g.pig.t.sign.SignDebugCommands;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resources.Identifier;
@@ -20,6 +21,7 @@ public final class GPigT implements ModInitializer {
         GPigTEntities.initialize();
         GPigTItems.initialize();
         SignAttachments.initialize();
+        SignDebugCommands.initialize();
         ResourceManagerHelper.get(PackType.SERVER_DATA)
                 .registerReloadListener(new NamesReloadListener());
     }
